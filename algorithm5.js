1ca16491365e87b7f8a0b7a437667dd1d89349e1
Amanual <amanual2@gmail.com>
2017-11-26 00:46:23 -0500

    algo5

---
+++ algorithm5.js
@@ -0,0 +1,11 @@
+//Assignment: Find Max
+var array1 = [-3, -2, 4, 7, 9, 10];
+var array2 = [];
+var max = 0;
+for (var i = 0; i < array1.length; i++) {
+    if (array1[i] > max) {
+        max = array1[i];
+
+    }
+
+}
